2f95df93e09d6817a735a9d5e1cd30b09bf3d5c5
Freddie Wu <freddie.wu@capitaledge.cn>
2017-03-29 21:42:17 +0800

    map textual tabs (\t) to &emsp; for better visual.

--- html/js/q-doc.js
+++ html/js/q-doc.js
@@ -47,22 +47,21 @@ QDoc.getFunctionSources = function() {
 //  @see QDoc.fileTreeDiv
 QDoc.buildFileTree = function(json) {
     QDoc.getHandlebarsTemplate("q-doc-file-tree.handlebars",
-		function(source) {
-			QDoc.fileTreeTemplate = Handlebars.compile(source);
-			QDoc.fileTree = QDoc.addElementsForFileTree(json);
+        function(source) {
+            QDoc.fileTreeTemplate = Handlebars.compile(source);
+            QDoc.fileTree = QDoc.addElementsForFileTree(json);
 
-			$("#" + QDoc.fileTreeDiv).html( QDoc.fileTreeTemplate(json) );
-		});
+            $("#" + QDoc.fileTreeDiv).html( QDoc.fileTreeTemplate(json) );
+        });
  }
 
 QDoc.addElementsForFileTree = function(filesJson) {
-    for(var fCount = 0; fCount < filesJson.files.length; fCount++) {
-        var file = filesJson.files[fCount].file;
-
-        filesJson.files[fCount].id = QDoc.escapeChars(file);
-        filesJson.files[fCount].link = "javascript:QDoc.get(\"" + file + "\")";
-    }
-
+    filesJson.files = $.map(filesJson.files, function(item) {
+        return $.extend(item, {
+            id: QDoc.escapeChars(item.file),
+            link: "javascript:QDoc.get(\"" + item.file + "\")"
+        });
+    });
     return filesJson;
  }
 
@@ -72,11 +71,11 @@ QDoc.get = function(file) {
     $("#" + QDoc.fileTreeDiv + "-group>a.active").removeClass("active");
     $("#" + QDoc.escapeChars(file)).addClass("active");
 
-	var url = "/jsn?.qdoc.json.getQDocFor`$\"" + file + "\"";
+    var url = "/jsn?.qdoc.json.getQDocFor`$\"" + file + "\"";
     $.getJSON(url, {},  QDoc.buildQDoc)
-		.fail(function(jq, status, error) {
-			console.error("JSON [" + url + "] " + status + ": " + error);
-		});
+        .fail(function(jq, status, error) {
+            console.error("JSON [" + url + "] " + status + ": " + error);
+        });
  }
 
 // Builds the HTML content page from the handlebars template and the post-processed
@@ -84,12 +83,12 @@ QDoc.get = function(file) {
 //  @see QDoc.postProcessDoc
 QDoc.buildQDoc = function(json) {
     QDoc.getHandlebarsTemplate("q-doc-element.handlebars",
-		function(source) {
-			var elementTemplate = Handlebars.compile(source);
-			var jsonUi = QDoc.postProcessDoc(json);
+        function(source) {
+            var elementTemplate = Handlebars.compile(source);
+            var jsonUi = QDoc.postProcessDoc(json);
 
-			$("#" + QDoc.contentDiv).html( elementTemplate(jsonUi) );
-		});
+            $("#" + QDoc.contentDiv).html( elementTemplate(jsonUi) );
+        });
  }
 
 
@@ -98,16 +97,16 @@ QDoc.buildQDoc = function(json) {
 // Function retrieves the specified template file, synchronously, from the server.
 //  @see QDoc.templatesLoc
 QDoc.getHandlebarsTemplate = function(templateFile, callback) {
-	var url = QDoc.templatesLoc + templateFile;
+    var url = QDoc.templatesLoc + templateFile;
     $.ajax({
         url: url,
-		dataType: "html",
+        dataType: "html",
         success: function(template) {
-			callback(template);
+            callback(template);
         },
-		error: function(jq, status, error) {
-			console.error("AJAX [" + url + "] " + status + ": " + error);
-		}
+        error: function(jq, status, error) {
+            console.error("AJAX [" + url + "] " + status + ": " + error);
+        }
     });
  }
 
@@ -120,8 +119,11 @@ QDoc.getHandlebarsTemplate = function(templateFile, callback) {
 //  - Format the types description
 QDoc.postProcessDoc = function(docJson) {
 
-    for(var dCount = 0; dCount < docJson.qdoc.length; dCount++) {
-        var element = docJson.qdoc[dCount];
+    function showTab(text) {
+        return text.replace(/\t/g, "\u2003");
+    }
+
+    docJson.qdoc = $.map(docJson.qdoc, function(element) {
         element.id = QDoc.escapeChars(element.func);
 
         if(S(element.func).contains("[")) {
@@ -130,23 +132,27 @@ QDoc.postProcessDoc = function(docJson) {
             element.arguments = "[" + element.arguments.join(";") + "]";
         }
 
-        element.comments = element.comments.join(" ");
+        element.comments = showTab(element.comments.join(" "));
         element.returns.description = S(element.returns.description).humanize();
 
-        for(var tCount = 0; tCount < element.throws.length; tCount++) {
-            element.throws[tCount].description = S(element.throws[tCount].description).humanize();
-        }
+        element.throws = $.map(element.throws, function(t) {
+            t.description = S(t.description).humanize();
+            return t;
+        });
 
-        for(var sCount = 0; sCount < element.see.length; sCount++) {
-            element.see[sCount] = {
-                linkOrFunc: element.see[sCount],
-                id: /^\s*<a[^>]+>[^<]+<\/a>\s*$/.test(element.see[sCount])
-                        ? null : QDoc.escapeChars(element.see[sCount])
+        element.see = $.map(element.see, function(s) {
+            return {
+                linkOrFunc: s,
+                id: /^\s*<a[^>]+>[^<]+<\/a>\s*$/.test(s) ? null : QDoc.escapeChars(s)
             };
-        }
+        });
 
-        docJson.qdoc[dCount] = element;
-    }
+        element.deprecated = $.map(element.deprecated, function(d) {
+            return showTab(d);
+        });
+
+        return element;
+    });
 
     return docJson;
  };
